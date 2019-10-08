1e8f110de3b90b9d8389d785d28f16d5f977e216
Hamid Mahmood <hmsulehri@yahoo.com>
2019-10-09 01:51:15 +0200

    Create variable for test data

--- Web/Tests/MenuTests.cs
+++ Web/Tests/MenuTests.cs
@@ -10,6 +10,8 @@ namespace Web.Tests
     [Parallelizable(ParallelScope.Fixtures)]
     public class MenuTests : TestBase
     {
+        private static readonly string ADDRESS = "Storgatan 1, Uppsala";
+
         public MenuTests(Browser browser) : base(browser) {}
 
         [Test]
@@ -30,7 +32,7 @@ namespace Web.Tests
 
             var broadbandPage = new BroadbandPage(driver);
             broadbandPage.ScrollToAddress();
-            broadbandPage.WriteAddress("Storgatan 1, Uppsala");
+            broadbandPage.WriteAddress(ADDRESS);
             broadbandPage.SelectAddressFromList();
             broadbandPage.SelectLghFromList();
         }
